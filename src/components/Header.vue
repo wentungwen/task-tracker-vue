7a8d0452ac2d4ee6f2907f11910bfab8b6030d8e
wentungwen <wentungwen@gmail.com>
2021-03-18 22:02:15 +0800

    half done

---
+++ src/components/Header.vue
@@ -0,0 +1,29 @@
+<template>
+  <header>
+    <h1>{{ title }}</h1>
+    <Button text="add task" color="green" />
+  </header>
+</template>
+<script>
+import Button from "./Button";
+export default {
+  name: "header",
+  props: {
+    title: {
+      type: String,
+      default: "hello!!",
+    },
+  },
+  components: {
+    Button,
+  },
+};
+</script>
+<style scoped>
+header {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  margin-bottom: 20px;
+}
+</style>
